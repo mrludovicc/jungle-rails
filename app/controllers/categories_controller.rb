12da239be0f55f89c08aca35cf33296eceb7e9e9
mrludovicc <nadal46@hotmail.com>
2023-04-05 19:51:03 -0400

    Add dashboard counts

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,9 @@
 class CategoriesController < ApplicationController
 
   def show
+    @categories_count = Category.count
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
   end
-
+puts "categories_count: #{@categories_count} categories"
 end
