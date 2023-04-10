66a8bc45fb0f9ea5ba6bc8d425158aac31f7d45f
mrludovicc <nadal46@hotmail.com>
2023-04-09 21:53:43 -0400

    Fix minor styling issue

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,16 +2,16 @@ module SessionsHelper
   def logged_in?
     session[:user_id].present?
   end
-
+  
   def logout_button
-    link_to "Logout", logout_path, method: :delete, class: "navbar-brand" if logged_in?
+    link_to "Logout", logout_path, method: :delete, class: "navbar-brand", style: "transition: color 0.5s ease-out;" if logged_in?
   end
-
+  
   def signup_button
-    link_to "Sign up", new_user_path, class: "navbar-brand" unless logged_in?
+    link_to "Sign up", new_user_path, class: "navbar-brand", style: "transition: color 0.5s ease-out;" unless logged_in?
   end
-
+  
   def login_button
-    link_to "Login", new_session_path, class: "navbar-brand" unless logged_in?
+    link_to "Login", new_session_path, class: "navbar-brand", style: "transition: color 0.5s ease-out;" unless logged_in?
   end
 end
